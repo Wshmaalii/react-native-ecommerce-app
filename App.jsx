20f4eac44128abbc27a3a55bd902b3146ad8c9aa
Washma Ali <wmis045@live.rhul.ac.uk>
2025-05-21 20:21:08 +0100

    Initial Expo app setup

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { Text } from 'react-native'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+        <Text>App</Text>
+    </NavigationContainer>
+    )
+}
+export default App
